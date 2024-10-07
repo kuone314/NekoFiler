bc608ae7603134b91cc233a550d8ce457544833c
kuone <kuone@example.com>
2024-10-07 16:58:58 +0900

    [feat] フィルタリング 大文字小文字の扱いを、変更
    * 小文字のみの場合、利便性を考慮して、大文字小文字を無視する。
    * 大文字を含む場合、意図的な入力と考え、大文字小文字を考慮する。

--- src-tauri/src/pane_info.rs
+++ src-tauri/src/pane_info.rs
@@ -1,4 +1,5 @@
 use std::{
+  borrow::Cow,
   collections::HashSet,
   fs::{self, Metadata},
   os::windows::fs::{FileTypeExt, MetadataExt},
@@ -141,46 +142,60 @@ impl FilterInfo {
       return Some(Vec::new());
     }
 
+    let exist_upper_case = self.matcher_str.chars().any(|c| c.is_uppercase());
+
+    let matcher_str = if !exist_upper_case {
+      Cow::Owned(self.matcher_str.to_lowercase())
+    } else {
+      Cow::Borrowed(&self.matcher_str)
+    };
+
+    let target = if !exist_upper_case {
+      Cow::Owned(target.to_lowercase())
+    } else {
+      Cow::Borrowed(target)
+    };
+
     match self.filter_type {
-      FilterType::StrMatch => self.str_match(&target),
-      FilterType::RegExpr => self.reg_expr_match(&target),
+      FilterType::StrMatch => str_match(&matcher_str, &target),
+      FilterType::RegExpr => reg_expr_match(&matcher_str, &target),
     }
   }
-  fn str_match(
-    &self,
-    target: &String,
-  ) -> MatchResult {
-    let mut matched_idx_list: Vec<usize> = Vec::new();
+}
 
-    for str_char in self.matcher_str.chars() {
-      let prev_match_idx = matched_idx_list.last().copied();
-      let search_start_idx = prev_match_idx.map_or(0, |idx| idx + 1);
-      let search_str = &target[search_start_idx..];
+fn str_match(
+  matcher_str: &String,
+  target: &String,
+) -> MatchResult {
+  let mut matched_idx_list: Vec<usize> = Vec::new();
 
-      if let Some(found_idx) = search_str.find(str_char) {
-        matched_idx_list.push(search_start_idx + found_idx);
-      } else {
-        return None;
-      }
+  for str_char in matcher_str.chars() {
+    let prev_match_idx = matched_idx_list.last().copied();
+    let search_start_idx = prev_match_idx.map_or(0, |idx| idx + 1);
+    let search_str = &target[search_start_idx..];
+
+    if let Some(found_idx) = search_str.find(str_char) {
+      matched_idx_list.push(search_start_idx + found_idx);
+    } else {
+      return None;
     }
-
-    Some(matched_idx_list)
   }
 
-  fn reg_expr_match(
-    &self,
-    target: &String,
-  ) -> MatchResult {
-    let Ok(reg_exp) = Regex::new(self.matcher_str.as_str()) else {
-      return None;
-    };
+  Some(matched_idx_list)
+}
 
-    let target = target.to_lowercase();
-    let Some(res) = reg_exp.find(target.as_str()) else {
-      return None;
-    };
-    Some((res.start()..res.end()).collect::<Vec<usize>>())
-  }
+fn reg_expr_match(
+  matcher_str: &String,
+  target: &String,
+) -> MatchResult {
+  let Ok(reg_exp) = Regex::new(matcher_str.as_str()) else {
+    return None;
+  };
+
+  let Some(res) = reg_exp.find(target.as_str()) else {
+    return None;
+  };
+  Some((res.start()..res.end()).collect::<Vec<usize>>())
 }
 
 #[derive(Debug, Clone)]
