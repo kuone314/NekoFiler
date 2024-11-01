551d975c42fb026300123448b4a6431fb065c23d
kuone <kuone@example.com>
2024-11-01 15:12:03 +0900

    ブランチルールの確認用コミット

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { ContextMenuSettingPane } from './ContextMenuSettingPane';
 import { FileListRowColorSettingPane } from './FileListRowColorSettingPane';
 import { ThemeProvider, useTheme } from './ThemeStyle';
 
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 const Mode = {
   main: "main",
