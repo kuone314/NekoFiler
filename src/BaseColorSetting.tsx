21beaee3cb5f0bdbd706902984ff50b9ad6163db
kuone <kuone@example.com>
2024-11-08 20:47:00 +0900

    [refactor] 配色設定 設定の保存,読み込みを、追加

--- src/BaseColorSetting.tsx
+++ src/BaseColorSetting.tsx
@@ -1,3 +1,5 @@
+import { ISettingInfo, writeSettings, readSettings } from "./ReadWriteSettings";
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 export type BaseColorSetting = {
   backgroundColor: string;
@@ -20,4 +22,30 @@ export function DefaultBaseColorSetting(): BaseColorSetting {
     stringErrorColor: '#ff0000',
   };
 }
+class Version {
+  static oldest = 1;
+  static latest = Version.oldest;
+}
+class SettingInfo implements ISettingInfo<BaseColorSetting> {
+  filePath = "general/base_color.json5";
+  latestVersion = Version.latest;
+  IsValidVersion = (version: number) => {
+    if (version < Version.oldest) { return false; }
+    if (version > Version.latest) { return false; }
+    return true;
+  };
+  UpgradeSetting = async (_readVersion: number, readSetting: BaseColorSetting) => {
+    let result = readSetting;
+    return result;
+  };
+}
 
+export async function writeBaseColor(setting: BaseColorSetting) {
+  writeSettings(new SettingInfo, setting);
+}
+
+export async function readBaseColor(): Promise<BaseColorSetting> {
+  const read = await readSettings(new SettingInfo);
+  const result = read ?? DefaultBaseColorSetting();
+  return result;
+}
